963003268931cf22741bf5621090968e41058061
NiMlr <nils.mueller-p43@rub.de>
2019-04-05 01:23:43 +0200

    stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 import os.path
 from setuptools import setup
 
-here = os.path.dirname(__file__)
-readme_path = os.path.join(here, 'README.md')
-readme = open(readme_path, 'rb').read().decode('utf-8')
+long_description = open('./README.md').read()
 
 setup(name='pyfunctionbases',
       use_scm_version={
@@ -11,7 +9,7 @@ setup(name='pyfunctionbases',
           'local_scheme': 'dirty-tag'
       },
       description='A Python module to compute multidimensional arrays of evaluated functions.',
-      long_description=readme,
+      long_description=long_description,
       url='https://github.com/NiMlr/PyFunctionBases',
       author='Nils Mueller',
       author_email='nils.mueller@ini.rub.de',
