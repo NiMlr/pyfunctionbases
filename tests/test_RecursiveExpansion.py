fix precision

tests/test_RecursiveExpansion.py
@@ -113,7 +113,11 @@ def test_RecursiveExpansionNode2():
         reshand = np.array([get_handcomputed_function_tensor(data[i, :], func, degree)
                             for i in range(data.shape[0])])
 
-        assert_array_almost_equal(resrec, reshand, DECIMAL-3)
+        if name == legendre_rational:
+            prec = 1
+        else:
+            prec = 0
+        assert_array_almost_equal(resrec, reshand, DECIMAL-3-prec)
         print('Multi dim ' + name + ' equal')
 
 